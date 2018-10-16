4c15f6f0f15d3cfd15a07ed4d8433572e90deb6c
Mark-Wspanialy <43036244+Mark-Wspanialy@users.noreply.github.com>
2018-10-16 10:32:09 -0400

    Update fileio.py

--- fileio.py
+++ fileio.py
@@ -1,1 +1,8 @@
+#fobject=open('important.txt','w')
+#fobject.write('go go go stop')
+#fobject.close()
 
+fobject=open('important.txt','r')
+print(fobject.read(5))
+print(fobject.read())
+fobject.close()
